b1bfd1725b3320a65836e157addbbf18867317e5
Dmitry Sherbakov <dvsherbakov@gmail.com>
2024-11-27 10:22:59 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {HashRouter, Link, Outlet, Route, Routes} from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <HashRouter>
+                <nav>
+                    <ul>
+                        <li><Link to={'/login'}>login</Link></li>
+                        <li><Link to={'/'}>home</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                <Route path='/' element={<Layout/>}>
+                        <Route index element={<div>Home </div>}/>
+                        <Route path='login' element={<div>Login</div>}/>
+                        <Route path='*' element={<NoMatch />} />
+                    </Route>
+                </Routes>
+            </HashRouter>
+        </div>
+    );
+}
+
+
+function Layout() {
+    return (
+        <>
+            <Outlet/>
+           <Footer />
+        </>
+    )
+}
+
+function NoMatch() {
+    return (
+        <div>
+            <h2>Nothing to see here!</h2>
+            <p>
+                <Link to='/'>Go to the home page</Link>
+            </p>
+        </div>
+    )
 }
 
 export default App;
+
+
